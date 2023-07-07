a78fc69db01f650eadefac85b6f3158895862524
Daniel R. Reynolds <reynolds@smu.edu>
2023-07-07 09:44:20 -0500

    Finished draft of initial demo -- has bugs that need fixing, and need to add plotting script

---
+++ initial_demo/GramSchmidt.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+#
+# Definition of a function in a separate file (Gram-Schmidt orthogonalization)
+#
+# D.R. Reynolds
+# Math 6321 @ SMU
+# Fall 2023
+
+# module imports
+import numpy as np
+
+def GramSchmidt(X):
+  """
+  Gram-Schmidt process for orthonormalizing a set of vectors.
+
+  Usage: iret,U = GramSchmidt(X)
+
+  Inputs:   X is a 2D ndarray object
+  Outputs:  iret is a success (0) or failure (1) flag
+            U is a 2D ndarray of the same shape as X, upon
+              successful completion, the columns of U form an
+              orthonormal basis for the column-space of X.
+  """
+
+  # copy input matrix
+  U = X.copy()
+
+  # check that there is work to do
+  if (np.size(U,1) < 1):
+    return [0, U]
+
+  # get entry magnitude (for linear dependence check)
+  Xmax = np.linalg.norm(U,np.inf)
+
+  # normalize first column
+  colnorm = np.linalg.norm(U[:,0])
+  if (colnorm < 1.e-13*Xmax):
+    print("GramSchmidt error: vectors are linearly-dependent!")
+    return [1, U]
+  U[:,0] /= colnorm
+
+  # iterate over remaining vectors, performing Gram-Schmidt process
+  for i in range(1,np.size(U,1)):
+
+    # subtract off portions in directions of existing basis vectors
+    for j in range(i):
+      U[:,i] -= (np.dot(U[:,i], U[:,j]) * U[:,j])
+
+    # normalize vector, checking for linear dependence
+    colnorm = np.linalg.norm(X[:,i])
+    if (colnorm < 1.e-13*Xmax):
+      print("GramSchmidt error: vectors are linearly-dependent!")
+      return [1, U]
+    U[:,i] /= colnorm
+
+  return [0,U]
--- initial_demo/demo.py
+++ initial_demo/demo.py
@@ -8,6 +8,7 @@
 
 # module imports
 import numpy as np
+from GramSchmidt import *
 
 # create a vector of length 5, and write this to screen
 a = np.ones(5)
@@ -37,18 +38,18 @@ for i in range(a.size):
 
 # write the values to file
 print("writing this same vector to the file 'a_data':")
-np.savetxt(a, "a_data")
+np.savetxt("a_data", a)
 
 # read vector from file
 tol = 2.e-15
-read_test1a = np.rand(3,4)
-np.savetxt(read_test1a, "tmp.txt")
+read_test1a = np.random.random_sample((3,4))
+np.savetxt("tmp.txt", read_test1a)
 read_test1b = np.loadtxt("tmp.txt")
 read_test1_error = read_test1a - read_test1b
-if (np.norm(read_test1_error,np.inf) < tol):
+if (np.linalg.norm(read_test1_error,np.inf) < tol):
   print("save/load test 1 passed")
 else:
-  print("save/load test 1 failed, ||error|| = ", np.norm(read_test1_error,np.inf))
+  print("save/load test 1 failed, ||error|| = ", np.linalg.norm(read_test1_error,np.inf))
   print("  read_test1a = ", read_test1a)
   print("  read_test1b = ", read_test1b)
 
@@ -66,15 +67,15 @@ print("   B = ", B)
 a[4] = 30.0  # reset to original
 
 # testing array slice constructor
-B2 = a[1:3]
-print("B2 = a[1:3] uses array slice constructor:")
+B2 = a[1:4]
+print("B2 = a[1:4] uses array slice constructor (note that it does not include the last index):")
 print(B2)
 # update entries of B2
 B2[0] = 4.0
 B2[1] = 3.0
 B2[2] = 2.0
 # copy B2 back into a using submatrix copy
-a[1:3] = B2
+a[1:4] = B2
 print("span copy back into a, should have entries 10 4 3 2 30")
 print(a)
 a[1] = 15.0  # reset to original
@@ -163,17 +164,17 @@ b = c
 print(b)
 
 print("Testing scalar fill, should be 3, 3, 3, 3, 3")
-a = 3*np.ones(a.size())
+a = 3*np.ones(a.size)
 print(a)
 
 print("Testing vector norm, should be 14.8324")
-print("  ", np.norm(b))
+print("  ", np.linalg.norm(b))
 
 print("Testing vector infinity norm, should be 10")
-print("  ", np.norm(b,np.inf))
+print("  ", np.linalg.norm(b,np.inf))
 
 print("Testing vector one norm, should be 30")
-print("  ", np.norm(b,1))
+print("  ", np.linalg.norm(b,1))
 
 print("Testing vector min, should be 2")
 print("  ", np.min(b))
@@ -181,8 +182,8 @@ print("  ", np.min(b))
 print("Testing vector max, should be 10")
 print("  ", np.max(b))
 
-B = np.zeros(2,5)
-B[0,0:4] = c  # B(0,:) = c
+B = np.zeros((2,5))
+B[0,0:5] = c  # B(0,:) = c
 B[1,:] = a    # B(1,:) = a
 B += 2.0
 
@@ -209,7 +210,7 @@ e = np.logspace(-2.0, 2.0, 5)
 print(e)
 
 # create and fill in a 10x5 matrix
-Y = np.zeros(10,5)
+Y = np.zeros((10,5))
 for i in range(10):
   Y[i,0] = 1.0*i
   Y[i,1] = -5.0 + 1.0*i
@@ -247,112 +248,55 @@ Y1 = np.abs(Y1)
 print("Testing abs, should be the column 5 4 3 2 1 0 1 2 3 4:")
 print(Y1)
 
-  // check the inplace_trans routine
-  cout << "Testing inplace_trans, should be the row 5 4 3 2 1 0 1 2 3 4:\n";
-  inplace_trans(Y1);
-  cout << Y1 << endl;
+# check the transpose routine
+print("Testing transpose:")
+Z = np.array([[1, 2, 3], [4, 5, 6]])
+print("Z =", Z)
+print("Z' = ", np.transpose(Z))
 
-  // check the copy-based transpose routine
-  cout << "Testing copy-based transpose, should be the column 5 4 3 2 1 0 1 2 3 4:\n";
-  Y2 = Y1.t();
-  cout << Y2 << endl;
+# check the Gram-Schmidt routine
+print("Testing GramSchmidt, should work")
+X = np.random.random_sample((20,3))
+iret,X = GramSchmidt(X)
+print("  GramSchmidt returned ", iret, ", dot-products are:")
+print("     <X0,X0> = ", np.dot(X[:,0],X[:,0]))
+print("     <X0,X1> = ", np.dot(X[:,0],X[:,1]))
+print("     <X0,X2> = ", np.dot(X[:,0],X[:,2]))
+print("     <X1,X1> = ", np.dot(X[:,1],X[:,1]))
+print("     <X1,X2> = ", np.dot(X[:,1],X[:,2]))
+print("     <X2,X2> = ", np.dot(X[:,2],X[:,2]))
 
-  cout << "Testing GramSchmidt, should work\n";
-  arma::mat X = arma::randu(20,3);
-  int iret = GramSchmidt(X);
-  cout << "  GramSchmidt returned " << iret << ", dot-products are:\n";
-  cout << "     <X0,X0> = " << arma::dot(X.col(0),X.col(0)) << endl;
-  cout << "     <X0,X1> = " << arma::dot(X.col(0),X.col(1)) << endl;
-  cout << "     <X0,X2> = " << arma::dot(X.col(0),X.col(2)) << endl;
-  cout << "     <X1,X1> = " << arma::dot(X.col(1),X.col(1)) << endl;
-  cout << "     <X1,X2> = " << arma::dot(X.col(1),X.col(2)) << endl;
-  cout << "     <X2,X2> = " << arma::dot(X.col(2),X.col(2)) << endl << endl;
+print("Testing GramSchmidt, should fail")
+V = np.random.random_sample((20,3))
+V[:,2] = 2*V[:,1]
+iret,V = GramSchmidt(V);
+print("  GramSchmidt returned ", iret, ", dot-products are:")
+print("     <V0,V0> = ", np.dot(V[:,0],V[:,0]))
+print("     <V0,V1> = ", np.dot(V[:,0],V[:,1]))
+print("     <V0,V2> = ", np.dot(V[:,0],V[:,2]))
+print("     <V1,V1> = ", np.dot(V[:,1],V[:,1]))
+print("     <V1,V2> = ", np.dot(V[:,1],V[:,2]))
+print("     <V2,V2> = ", np.dot(V[:,2],V[:,2]))
 
-  cout << "Testing GramSchmidt, should fail\n";
-  arma::mat V = arma::randu(20,3);
-  V.col(2) = 2.0*V.col(1);
-  iret = GramSchmidt(V);
-  cout << "  GramSchmidt returned " << iret << ", dot-products are:\n";
-  cout << "     <V0,V0> = " << arma::dot(V.col(0),V.col(0)) << endl;
-  cout << "     <V0,V1> = " << arma::dot(V.col(0),V.col(1)) << endl;
-  cout << "     <V0,V2> = " << arma::dot(V.col(0),V.col(2)) << endl;
-  cout << "     <V1,V1> = " << arma::dot(V.col(1),V.col(1)) << endl;
-  cout << "     <V1,V2> = " << arma::dot(V.col(1),V.col(2)) << endl;
-  cout << "     <V2,V2> = " << arma::dot(V.col(2),V.col(2)) << endl << endl;
+print("Testing matrix product, should be: 9 -1 9 -8 11 6")
+A_ = np.eye(6)
+A_[0,3] = 2.0
+A_[1,2] = -1.0
+A_[2,5] = 1.0
+A_[3,5] = -2.0
+A_[4,5] = 1.0
+xtrue_ = np.linspace(1.0, 6.0, 6)
+b_ = A_@xtrue_
+print(b_)
 
-  cout << "Testing matrix product, should be: 9 -1 9 -8 11 6\n";
-  arma::mat A_ = arma::eye(6,6);
-  A_(0,3) = 2.0;
-  A_(1,2) = -1.0;
-  A_(2,5) = 1.0;
-  A_(3,5) = -2.0;
-  A_(4,5) = 1.0;
-  arma::mat xtrue_ = arma::linspace(1.0, 6.0, 6);
-  arma::mat b_ = A_*xtrue_;
-  cout << b_ << endl;
-
-  cout << "Testing backwards substitution solve with provided solution array:\n";
-  arma::vec x_(6);
-  if (arma::solve(x_, A_, b_)) {
-    cout << "  solve succeeded\n";
-  } else {
-    cout << "  solve failed\n";
-  }
-  cout << "  ||x - xtrue|| = " << arma::norm(x_ - xtrue_, "inf") << "\n\n";
-
-  cout << "Testing forwards substitution with provided solution array:\n";
-  A_.eye();
-  A_(3,0) = 2.0;
-  A_(2,1) = -1.0;
-  A_(5,2) = 1.0;
-  A_(5,3) = -2.0;
-  A_(5,4) = 1.0;
-  b_ = A_*xtrue_;
-  x_ = 0.0;
-  if (arma::solve(x_, A_, b_)) {
-    cout << "  solve succeeded\n";
-  } else {
-    cout << "  solve failed\n";
-  }
-  cout << "  ||x - xtrue|| = " << arma::norm(x_ - xtrue_, "inf") << "\n\n";
-
-  cout << "Testing general solver:\n";
-  arma::mat C_ = 100.0*arma::eye(9,9) + arma::randu(9,9);
-  arma::vec z_ = arma::logspace(-4.0, 4.0, 9);
-  arma::mat f_ = C_*z_;
-  arma::mat g_ = arma::solve(C_, f_);
-  cout << "  ||x - xtrue|| = " << arma::norm(g_ - z_, "inf") << "\n\n";
-
-  cout << "Testing copy-into-col, should be: \n";
-  cout << "    0.01   0.02   0.04   0.08\n";
-  cout << "    0.1    0.2    0.4    0.8\n";
-  cout << "    1      2      4      8\n";
-  cout << "   10     20     40     80\n";
-  cout << " Actually is:\n";
-  arma::mat z2_ = arma::logspace(-2.0, 1.0, 4);
-  arma::mat B_(4,4);
-  B_.col(0) = z2_;
-  z2_ *= 2.0;
-  B_.col(1) = z2_;
-  z2_ *= 2.0;
-  B_.col(2) = z2_;
-  z2_ *= 2.0;
-  B_.col(3) = z2_;
-  cout << B_ << endl;
-
-  cout << "Testing general solver with matrix-valued rhs:\n";
-  arma::mat E_ = 100.0*arma::eye(4,4) + arma::randu(4,4);
-  arma::mat F_ = E_*B_;
-  arma::mat X_ = arma::solve(E_, F_);
-  cout << "  ||X - Xtrue|| = " << arma::norm(X_ - B_,"inf") << "\n\n";
-
-  cout << "Testing matrix inverse:\n";
-  arma::mat D_ = 10.0*arma::eye(8,8) + arma::randu(8,8);
-  arma::mat DDinv_(D_);
-  arma::mat Dinv_ = D_.i();
-  DDinv_ = D_*Dinv_;
-  cout << "  ||I - D*Dinv|| = " << arma::norm(arma::eye(8,8) - DDinv_,"inf") << endl;
-  DDinv_ = Dinv_*D_;
-  cout << "  ||I - Dinv*D|| = " << arma::norm(arma::eye(8,8) - DDinv_,"inf") << endl;
-
-  return 0;
+print("Testing direct linear solver:")
+C_ = 100*np.eye(9) + np.random.random_sample((9,9))
+z_ = np.logspace(-4.0, 4.0, 9)
+b_ = C_@z_
+try:
+  x_ = np.linalg.solve(C_, b_)
+except:
+  print("  solve failed")
+else:
+  print("  solve succeeded")
+print("  ||x - xtrue|| = ", np.linalg.norm(x_ - z_, np.inf), "\n\n")
